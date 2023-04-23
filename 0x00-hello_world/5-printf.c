891eeae9cf766cef52bc73fcb39fce4f5cf0a5f0
shaimaaessam24 <shaimaaessam236@gmail.com>
2023-04-23 16:18:05 -0700

    sixth task

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* descriotion: A C program that prints with printf function.
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
