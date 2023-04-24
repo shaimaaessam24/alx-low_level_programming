7f746f1aacd1400e509b77e4f466cf83a2cc8326
shaimaaessam24 <shaimaaessam236@gmail.com>
2023-04-24 13:27:59 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
-#include <untstd.h>
+#include <unistd.h>
 /**
- * main - A c program that prints a line to the standard error
- * Return 1 
- * (success)
+ * main - Entry point
+ * description: print a qoute using write function
+ * ssize_t write(int (fd.const void *buf.size_t count);
+ * Return: 1 (error).
  */
 int main(void)
-{
-fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+{char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(1, quo, 59);
 return (1);
 }
